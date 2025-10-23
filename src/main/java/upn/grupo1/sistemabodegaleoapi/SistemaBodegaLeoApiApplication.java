1029cfc027f0ff8f003707951d2a26f9e31f8a3b
ginoalejandro <ginoalejandro2304@gmail.com>
2025-10-22 23:36:32 -0500

    creacion de proyecto

---
+++ src/main/java/upn/grupo1/sistemabodegaleoapi/SistemaBodegaLeoApiApplication.java
@@ -0,0 +1,13 @@
+package upn.grupo1.sistemabodegaleoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaBodegaLeoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaBodegaLeoApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/upn/grupo1/sistemabodegaleoapi/SistemaBodegaLeoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package upn.grupo1.sistemabodegaleoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaBodegaLeoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
